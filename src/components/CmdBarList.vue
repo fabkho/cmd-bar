3463b34f4be276b92894365c94f1986d92fb9b5a
Fabian Kirchhoff <fabian.kirchhoff@anny.co>
2023-09-12 19:26:31 +0200

    fix: virtualList not working for nested lists

--- src/components/CmdBarList.vue
+++ src/components/CmdBarList.vue
@@ -1,8 +1,7 @@
 <script setup lang="ts">
 import { useCmdBarState } from '@/useCmdBarState'
-import { computed, watch, type ComputedRef } from 'vue'
+import { computed, watch, ref, nextTick } from 'vue'
 import CmdBarGroup from '@/components/CmdBarGroup.vue'
-import { useVirtualList } from '@vueuse/core'
 import type { Group } from '@/types'
 
 const props = defineProps<{
@@ -10,38 +9,41 @@ const props = defineProps<{
   containerHeight: string
 }>()
 
+const containerRef = ref<HTMLElement | null>(null)
+
 const groupedCommands = computed(() => {
   return useCmdBarState.state.filteredGroupedCommands as Group[]
 })
 
-const visibleItems = computed(() => {
-  return useCmdBarState?.state.commands
-})
-
-const { list, containerProps, wrapperProps } = useVirtualList(visibleItems as ComputedRef, {
-  itemHeight: props.itemHeightInPixel
-})
+// const visibleItems = computed(() => {
+//   return useCmdBarState?.state.commands
+// })
+//
+// const { list, containerProps, wrapperProps } = useVirtualList(visibleItems as ComputedRef, {
+//   itemHeight: props.itemHeightInPixel
+// })
 
 // set fixed height for container
 watch(
   () => props.containerHeight,
   () => {
-    containerProps.style = {
-      height: props.containerHeight
-    }
+    nextTick(() => {
+      if (containerRef.value) {
+        containerRef.value.style.height = props.containerHeight
+      }
+    })
   },
   { immediate: true }
 )
 </script>
 
 <template>
-  <div class="grouped-list" v-bind="containerProps">
-    <ul data-cmd-bar-items class="list-items" v-bind="wrapperProps">
+  <div class="grouped-list" ref="containerRef">
+    <ul data-cmd-bar-items class="list-items">
       <li v-for="group in groupedCommands" :key="group.key" class="group">
         <h2 v-if="group.label && group.commands.length > 0" class="group__label">
           {{ group.label }}
         </h2>
-        <pre>{{ list }}</pre>
         <CmdBarGroup :group="group">
           <template v-for="(_, name) in $slots" #[name]="slotData">
             <slot :name="name" v-bind="slotData" />
