cf449bd3e26edf8849ebda94dc78285f355ce612
Fabian Kirchhoff <fabian.kirchhoff@anny.co>
2023-12-15 11:44:06 +0100

    feat(cmd-bar): new example with checkbox

---
+++ packages/examples/checkbox/app.vue
@@ -0,0 +1,15 @@
+<template>
+  <header></header>
+
+  <main>
+    <client-only>
+      <CmdBar />
+    </client-only>
+  </main>
+</template>
+
+<script setup lang="ts">
+import CmdBar from './components/CmdBar.vue'
+</script>
+
+<style scoped></style>
