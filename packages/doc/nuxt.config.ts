b096b936bf81ecd5d0fafaede8242e6177956027
Fabian Kirchhoff <fab.kirchhoff98@gmail.com>
2024-09-20 18:00:03 +0200

    docs: linting

--- packages/doc/nuxt.config.ts
+++ packages/doc/nuxt.config.ts
@@ -1,13 +1,9 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  modules: [
-    '@nuxt/content',
-    '@nuxtjs/tailwindcss',
-    'nuxt-headlessui',
-  ],
+  modules: ['@nuxt/content', '@nuxtjs/tailwindcss', 'nuxt-headlessui'],
 
   headlessui: {
-    prefix: 'Headless',
+    prefix: 'Headless'
   },
 
   app: {
@@ -17,29 +13,31 @@ export default defineNuxtConfig({
       meta: [
         { charset: 'utf-8' },
         { name: 'viewport', content: 'width=device-width, initial-scale=1, maximum-scale=1' },
-        { name: 'description', content: 'This template is designed to help you create the most effective documentation for your projects' },
+        {
+          name: 'description',
+          content:
+            'This template is designed to help you create the most effective documentation for your projects'
+        },
         { name: 'format-detection', content: 'telephone=no' },
-        { key: 'og:image', property: 'og:image', content: '/og-image.webp' },
+        { key: 'og:image', property: 'og:image', content: '/og-image.webp' }
       ],
-      link: [
-        { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
-      ],
-    },
+      link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }]
+    }
   },
 
   routeRules: {
-    '/': { prerender: true },
+    '/': { prerender: true }
   },
 
   content: {
     highlight: {
       theme: {
-        default: 'github-dark',
-      },
-    },
+        default: 'github-dark'
+      }
+    }
   },
 
   compatibilityDate: '2024-09-04',
 
-  devtools: { enabled: true },
+  devtools: { enabled: true }
 })
